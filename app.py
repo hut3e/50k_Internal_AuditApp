90e664e54753a06ae4ec18e562debec6edbab87a
TNV <76718994+toanvn85@users.noreply.github.com>
2025-05-22 16:13:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,9 +17,10 @@ from stats_dashboard import stats_dashboard
 from admin_dashboard import admin_dashboard
 from database_helper import (
     get_supabase_client, 
-    check_supabase_config,
-    check_email_exists,
-    get_user
+    check_supabase_config, 
+    get_user, 
+    register_user, 
+    check_email_exists
 )
 from PIL import Image, UnidentifiedImageError
 
